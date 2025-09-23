f16d9ec050d18cc62d8e3bb709389e4071dc8156
khushi guptaa <khushigupta1830@gmail.com>
2025-09-23 19:56:18 +0000

    small details

---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const  accountNumber= 9999
+let accountEmail="khushigupta1830@gmail.com"
+var acountCity = "indore"
+var acountCity="delhi"
+
+var acountCity="Ranchi"
+
+ // preefer not to use var due to issue in block scope and functiona scope 
+
+
+ let accountId = "khushigupta"  //without any return type cis undefined 
+
+ let accountHolder;
+console.table([accountEmail,accountNumber,acountCity,accountId]);
+
+
