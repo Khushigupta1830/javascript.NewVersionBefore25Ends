ee33413f9bd7ce5c3a8a8cb1ed91ca8704661e07
khushi guptaa <khushigupta1830@gmail.com>
2025-10-04 13:24:28 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,47 @@
+"use strict"; //   treat all js code as newer version 
+
+
+ //alert (9+4) //     (we are using node js nt browser 
+ 
+  // in coding readablility is important not the (:; not )
+  // code should be future prove also therfore we need a good readablilty 
+
+   let age= 18 
+   let value= 18.24
+   let boolean = false 
+  console.log("khushi");             // right 
+  console.log(8+9);                  //right
+  
+
+  
+// number =>2 to the power 53
+// bigint
+//string =>""
+//boolean => true /false 
+//null => standalone value => representation of empty
+// undefined => when the value is not asigned 
+/* ex:
+       let state:
+       let AccountCity:
+
+ */
+ // symbol => to find uniquness in the react specially 
+ 
+
+console.log(typeof "khushi");
+console.log(typeof 8+9 );
+console.log(typeof age);
+console.log(typeof value);
+console.log(typeof boolean);
+console.log(typeof null);          //     OBJECT  // << interview question>> //
+console.log(typeof undefined );   // undefined
+
+
+
+
+
+
+
+
+
+
