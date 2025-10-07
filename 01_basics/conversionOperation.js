d4c43f53933b32784aad69d84a5e8e516657a98d
khushi guptaa <khushigupta1830@gmail.com>
2025-10-07 10:52:56 +0000

    convwersionsOperations

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,47 @@
+// let score = "33"
+// console.log( typeof score)   // number 
+// console.log(typeof (score))  // wrote it in method form answer: number
+//  let valueInNumber = Number(score) // valuInNumber ko hmne Number put kr diya 
+
+//  console.log(typeof valueInNumber);  // Ab yahn find kr rhe , ki valueInNumber => Number 
+//  console.log(valueInNumber); // ab yahn puch rhe hai ki value kya hai that is number => 33 
+
+
+ 
+
+
+
+
+
+//let score = "33abc"
+
+//console.log(typeof score);           // string
+//console.log(typeof (score));        // string 
+
+ //let ValueInNumber = Number(score) 
+//console.log(typeof ValueInNumber);   // number 
+ //console.log(ValueInNumber);    // it will print what is inside valueInNumber i.e NaN
+
+// "33" => 33
+// "33abc" => NaN
+// TRUE => 1 , FALSE => 0 .
+
+
+ //let IsLoggedIn= 1
+ //let  booleanIsLoggedIn = Boolean(IsLoggedIn) ;  // define kara hai    // true
+
+ //console.log(booleanIsLoggedIn);
+            
+   // 1 => true
+   // 0 => false 
+   // "khushi" =>  true 
+ s
+   let someNumber = 33
+   let stringNumber = String(someNumber)
+   console.log(stringNumber);
+   console.log(typeof stringNumber);
+   
+
+
+
+ 
