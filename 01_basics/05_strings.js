41890f6f82106ae74755b5dafe1c522f986c7961
khushi guptaa <khushigupta1830@gmail.com>
2025-10-10 21:42:11 +0000

    strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,4 +1,4 @@
-const name ="khushi"
+const name ="khushi"  
 const repocount = 45
 // console.log(name +  repocount + "value");
 
@@ -6,4 +6,19 @@ const repocount = 45
 console.log(`hello my name is ${name}  and my repo count is ${repocount}`);
 
 //  how to define a string  , 
-const gamename = new string (`hiteshhc`)
+const gamename = new String('hiteshhc')           
+// console log mei jaakr ke hm dekh sakte hai that it forms , [0] 'h'
+// [1] 'i'
+// [2] 't'
+// [3]'e'
+// [4]'s'
+ console.log(gamename[0]);
+ console.log(gamename[1]);
+ console.log(gamename[2]);
+ console.log(gamename[3]);
+ console.log(gamename[4]);
+ console.log(gamename[5]);
+ console.log(gamename.__proto__);     /*gives {}  */
+ console.log(gamename.length);            /*gives length  */
+ 
+ 
