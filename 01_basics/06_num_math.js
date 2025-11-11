53cf8f589a49077dc514002dd63e2f584eab1b58
khushi guptaa <khushigupta1830@gmail.com>
2025-11-11 20:02:53 +0000

    math ceil floor min max

--- 01_basics/06_num_math.js
+++ 01_basics/06_num_math.js
@@ -5,17 +5,39 @@ console.log(score);
 const balance = new Number(100) ;            // specially caste that its a number...
                                          
 console.log(balance); 
-console.log(balance.toString());        // here the number is converted to string type
+// console.log(balance.toString());        // here the number is converted to string type
                                           //and hence in string type we know 
                                // there is a lot of propery..
 console.log(balance.toFixed(4));
 
 const othernumber = 12.8933 ;
-console.log(othernumber.toPrecision(2));   // use toPricision keyword very attentively while coding as it / carryfully 
+// console.log(othernumber.toPrecision(2));   // use toPricision keyword very attentively while coding as it / carryfully 
 
 const hundreds = 10000000
 
-console.log(hundreds.toLocaleString('en-IN')); // CONVERT TO comma in indian rupees
+// console.log(hundreds.toLocaleString('en-IN')); // CONVERT TO comma in indian rupees
+
+  //                          local number mei isko print krega as indian 
+//_+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);             // object [math] {}
+// console.log(Math.abs);              // neg to positive..
+
+// console.log(Math.round(9.7));     // round off krta hai number ka..
+// console.log(Math.ceil(9.7));       // upper number 
+// console.log(Math.floor(9.7));       // lower number 
+// console.log(Math.min(9,8, 7, 6 , 5 , 4));          // array mei check krta hai that minimum number 
+// console.log(Math.max(9, 8 , 7, 6, 5,4));           // check maximum number 
+ 
+console.log(Math.random());                         // output between , 0 - 1 
+
+console.log((Math.random()*10) + 1 );              // output , diff range 
+console.log((Math.random()*10) + 1 );
+console.log(Math.floor(Math.random()*10) + 1 );     // we used the math floor to wrap up for the min number 
+
+
+const min  = 10                                     // for minimum for the given  number 
+const max = 90                                     // for maximum 
 
 
 
