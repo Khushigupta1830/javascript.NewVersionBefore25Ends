7859392cb2902db8f62f85e8e3de0a82505fe334
khushi guptaa <khushigupta1830@gmail.com>
2025-11-11 18:10:19 +0000

    ss

--- 01_basics/06_num_math.js
+++ 01_basics/06_num_math.js
@@ -1,5 +1,8 @@
-const score = 400
-const balance = new Number(100)
+const score = 400 ;
+console.log(score);
+
+const balance = new Number(100) ;
                                          // definately it will be  number 
 console.log(balance); 
+
 
