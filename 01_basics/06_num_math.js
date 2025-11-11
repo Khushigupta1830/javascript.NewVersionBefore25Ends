7fff3413441ab2f59256dac6406c5a55166d9c1c
khushi guptaa <khushigupta1830@gmail.com>
2025-11-11 20:34:19 +0000

    math and num

--- 01_basics/06_num_math.js
+++ 01_basics/06_num_math.js
@@ -1,14 +1,14 @@
 const score = 400 ;                // ye phle se he number hai maan liya hai...
-
-console.log(score);
+// 
+// console.log(score);
 
 const balance = new Number(100) ;            // specially caste that its a number...
                                          
-console.log(balance); 
+// console.log(balance); 
 // console.log(balance.toString());        // here the number is converted to string type
                                           //and hence in string type we know 
                                // there is a lot of propery..
-console.log(balance.toFixed(4));
+// console.log(balance.toFixed(4));
 
 const othernumber = 12.8933 ;
 // console.log(othernumber.toPrecision(2));   // use toPricision keyword very attentively while coding as it / carryfully 
@@ -29,15 +29,19 @@ const hundreds = 10000000
 // console.log(Math.min(9,8, 7, 6 , 5 , 4));          // array mei check krta hai that minimum number 
 // console.log(Math.max(9, 8 , 7, 6, 5,4));           // check maximum number 
  
-console.log(Math.random());                         // output between , 0 - 1 
+// console.log(Math.random());                         // output between , (0 - 1 ) 
 
-console.log((Math.random()*10) + 1 );              // output , diff range 
-console.log((Math.random()*10) + 1 );
-console.log(Math.floor(Math.random()*10) + 1 );     // we used the math floor to wrap up for the min number 
-
-
-const min  = 10                                     // for minimum for the given  number 
-const max = 90                                     // for maximum 
+// console.log((Math.random()*10) + 1 );              // output , should be greater than 1.. 
+// console.log((Math.random()*10) + 1 );
+// console.log(Math.floor(Math.random()*10) + 1 );     // we used the math floor to wrap up for the min number /// min floor hme zero e skta h but at the end we do + 1..
 
 
+const min  = 87                                            // for minimum for the given  number 
 
+const max = 90                                             // for maximum 
+ 
+console.log(Math.floor(Math.random()* (max - min +  1))+ min )
+
+// Math.floor ==> single digit number milne ke liye 
+//  (max - min +  1) ==> just to avoid zero As the output 
+// +min ==> done , to get output more th
