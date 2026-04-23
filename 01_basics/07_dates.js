3896d28a4c78016b398525e2dc42d1797bfa29c3
khushi guptaa <khushigupta1830@gmail.com>
2026-04-23 18:14:32 +0000

    newdate

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -7,25 +7,31 @@ let mydate = new Date(23);
  
 
 let myCreatedDate = new Date (23 , 4 , 2026);
-console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toDateString());
 
 
 let myCreatedDateTwo = new Date (23 , 4 , 2026 , 3, 17 );
-console.log(myCreatedDate.toLocaleString());                    // 11/15/1928, 12:00:00 AM
+// console.log(myCreatedDate.toLocaleString());                    // 11/15/1928, 12:00:00 AM
  
 let mytimestamp = Date.now();
-console.log(mytimestamp);                     // long digit aagye Hai ..
+// console.log(mytimestamp);                     // long digit aagye Hai ..
 
 
 // console.log (myCreatedDate.getTime());
 
-console.log(Math.floor(Date.now()/1000));
+// console.log(Math.floor(Date.now()/1000));
 
 let newdate = new Date();
-console.log(newdate);
+// console.log(newdate);
 
 console.log(newdate.getMinutes());
+console.log(newdate.getTime());
+console.log(newdate.getMonth()+1);
 
 
+console.log(newdate.getHours);
 
-
+newdate.toLocaleString();
+newdate.toLocaleString('default ', {
+    weekday : "long ",})
+console.log(newdate);
