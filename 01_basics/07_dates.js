bb3b7cd2e734f77ecd45f65496fa148eaa175482
khushi guptaa <khushigupta1830@gmail.com>
2026-04-23 12:52:43 +0000

    Maths and Dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,31 @@
+let mydate = new Date(23);
+// console.log(mydate.toISOString());                  
+// console.log(mydate.toJSON());                      //1970-01-01T00:00:00.023Z
+// console.log(mydate.toLocaleString());             //1/1/1970, 12:00:00 AM
+// console.log(mydate.toDateString());              // Thu Jan 01 1970
+// console.log(typeof mydate );                    // object 
+ 
+
+let myCreatedDate = new Date (23 , 4 , 2026);
+console.log(myCreatedDate.toDateString());
+
+
+let myCreatedDateTwo = new Date (23 , 4 , 2026 , 3, 17 );
+console.log(myCreatedDate.toLocaleString());                    // 11/15/1928, 12:00:00 AM
+ 
+let mytimestamp = Date.now();
+console.log(mytimestamp);                     // long digit aagye Hai ..
+
+
+// console.log (myCreatedDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
+
+let newdate = new Date();
+console.log(newdate);
+
+console.log(newdate.getMinutes());
+
+
+
+
