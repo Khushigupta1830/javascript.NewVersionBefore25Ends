e534c24c844e23ae1f867b6586240ed8eb3beaa8
khushi guptaa <khushigupta1830@gmail.com>
2026-05-07 17:45:02 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -21,7 +21,28 @@ const regularUser={
     }
 }
 
-console.log(regularUser);                        // fullname: { username: { firstname: 'khushi', lastname: 'gupta' } }
-console.log(regularUser.fullname);
+//console.log(regularUser);                        // fullname: { username: { firstname: 'khushi', lastname: 'gupta' } }
+//console.log(regularUser.fullname);
                                                  //{ username: { firstname: 'khushi', lastname: 'gupta' } }
+const obj1 ={1: "A" , 2:"B"};
+const obj2 ={3: "A" , 4:"B"};
+//const obj3 = Object.assign({} , obj1, obj2);    // object assign is a static method , which copies the enumeral own properties ..
+const obj3 = {...obj1 , ...obj2};
+console.log(obj3);                                  // spread fconcept we use here  /// const obj1 ={"1": "A" , "2":"B"};
+
+const users = [
+    {
+     id : 1,
+     email : "khushi@gmail.com" ,
+     address : "abcde1234" ,
+
+     
+    }
+]
+
+    users[1].email
+    console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+
 
