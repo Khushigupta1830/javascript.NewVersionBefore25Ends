ac6efb9088d40a76fe0506f1faccd2a3872646ee
khushi guptaa <khushigupta1830@gmail.com>
2026-05-10 10:03:37 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,18 @@
+function SayMyName(){
+    console.log("K");
+    console.log("H");
+    console.log("U");
+    console.log("S");
+    console.log("H");
+    console.log("I");
+    
+    
+}
+SayMyName()
+
+
+function addTwoNumber(number1 , number2){
+    console.log(number1 + number2);
+    
+}
+addTwoNumber(24 , "s")
